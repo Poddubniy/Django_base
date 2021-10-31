4662d154e7a950cf63d9e052ec0cf74023c77b0a
Vasiliy <vasiliypoddubny@gmail.com>
2021-10-31 22:35:07 +0300

    Added user_passes_test

--- admins/views.py
+++ admins/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {
         'title': 'Geekshop - Admin panel',
@@ -13,6 +15,7 @@ def index(request):
 
 
 # Create
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -30,6 +33,7 @@ def admin_users_create(request):
 
 
 # Read
+@user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     context = {
         'title': 'Geekshop - Users',
@@ -39,6 +43,7 @@ def admin_users(request):
 
 
 # Update
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_update(request, id):
     selected_user = User.objects.get(id=id)
     if request.method == 'POST':
@@ -58,7 +63,8 @@ def admin_users_update(request, id):
 
 
 # Delete
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_delete(request, id):
     user = User.objects.get(id=id)
-    user.delete()
+    user.safe_delete()
     return HttpResponseRedirect(reverse('admins:admin_users'))
