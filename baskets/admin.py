263abeb2ec3704070fe78158d4a54d1696ca75ff
Vasiliy <vasiliypoddubny@gmail.com>
2021-10-31 23:06:59 +0300

    Added class for baskets, users in admin.py

--- baskets/admin.py
+++ baskets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from baskets.models import Basket
 
-# Register your models here.
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')  # помещая в один кортеж, поля размещаются на одной линии
+    extra = 0  # убираем лишнии поля
